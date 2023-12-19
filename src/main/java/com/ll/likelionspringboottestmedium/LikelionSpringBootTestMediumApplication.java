bb2ff79cf6bb94b5d3f3371875121514e32d8132
mabyoungg <mabyoungg@gmail.com>
2023-12-19 23:54:56 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/ll/likelionspringboottestmedium/LikelionSpringBootTestMediumApplication.java
@@ -0,0 +1,13 @@
+package com.ll.likelionspringboottestmedium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionSpringBootTestMediumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikelionSpringBootTestMediumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/likelionspringboottestmedium/LikelionSpringBootTestMediumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.likelionspringboottestmedium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikelionSpringBootTestMediumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

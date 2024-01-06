726773f56ba5afe1d3987d48809e3aebcd5f8abc
mabyoungg <mabyoungg@gmail.com>
2024-01-06 19:58:42 +0900

    feat: 로그인

--- src/main/java/com/ll/likelionspringboottestmedium/domain/memeber/memeber/controller/MemberController.java
+++ src/main/java/com/ll/likelionspringboottestmedium/domain/memeber/memeber/controller/MemberController.java
@@ -42,4 +42,9 @@ public class MemberController {
 
         return rq.redirectOrBack(joinRs, "/member/login");
     }
+
+    @GetMapping("/login")
+    public String showLogin() {
+        return "domain/member/member/login";
+    }
 }
--- src/main/java/com/ll/likelionspringboottestmedium/domain/memeber/memeber/service/MemberService.java
+++ src/main/java/com/ll/likelionspringboottestmedium/domain/memeber/memeber/service/MemberService.java
@@ -4,6 +4,7 @@ import com.ll.likelionspringboottestmedium.domain.memeber.memeber.entity.Member;
 import com.ll.likelionspringboottestmedium.domain.memeber.memeber.repository.MemberRepository;
 import com.ll.likelionspringboottestmedium.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +16,7 @@ import java.util.Optional;
 @Transactional(readOnly = true)
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public RsData<Member> join(String username, String password) {
@@ -25,8 +27,8 @@ public class MemberService {
         if (findByUsername(username).isPresent()) {
             return RsData.of("400-2", "이미 존재하는 회원입니다.");
         }
-        
-        Member member = new Member(username, password);
+
+        Member member = new Member(username, passwordEncoder.encode(password));
         memberRepository.save(member);
 
         return RsData.of("200", "%s님 환영합니다. 회원가입이 완료되었습니다. 로그인 후 이용해주세요.".formatted(member.getUsername()), member);

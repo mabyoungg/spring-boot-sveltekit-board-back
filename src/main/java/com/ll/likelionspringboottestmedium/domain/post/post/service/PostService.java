167243980a90bc13f64495ee608499459084eb63
mabyoungg <mabyoungg@gmail.com>
2024-01-06 23:33:27 +0900

    feat: 글 상세 페이지

--- src/main/java/com/ll/likelionspringboottestmedium/domain/post/post/service/PostService.java
+++ src/main/java/com/ll/likelionspringboottestmedium/domain/post/post/service/PostService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -29,5 +30,9 @@ public class PostService {
 
     public List<Post> findTop30ByIsPublishedOrderByIdDesc(boolean isPublished) {
         return postRepository.findTop30ByIsPublishedOrderByIdDesc(isPublished);
+    }
+
+    public Optional<Post> findById(long id) {
+        return postRepository.findById(id);
     }
 }
